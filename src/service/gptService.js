614cb4ae56a5c942d76472230c30cf4d2595ea7a
Sidney Junio Fortes Goncalves <sidneyjunio191@gmail.com>
2024-12-12 13:01:26 -0300

    Fix: Corrigido erro de tratamento de exceção no gptService para melhor lidar com falhas de conexão.

--- src/service/gptService.js
+++ src/service/gptService.js
@@ -1,5 +1,6 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const env = require('../utils/envLoader'); // Carrega variáveis de ambiente
+const chalk = require("chalk");
 
 // Configure o Google Generative AI
 const genAI = new GoogleGenerativeAI(env.GEMINI_API_KEY); // Use sua chave de API do Gemini
@@ -8,12 +9,14 @@ const generateCommitMessage = async (changes) => {
   try {
     // Configuração do modelo
     const model = await genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-    const prompt = `Generate a concise and clear commit message based on these changes:\n${changes}`;
+    const prompt = `Generate a concise, one-line commit message in Portuguese that uses professional commit prefixes like "Fix", "Refactor", "Style", or others, followed by a clear explanation of what was changed and why, ensuring the message is easily understandable for developers reviewing the commit:\n${changes}`;
 
     // Gera o conteúdo com base no prompt
+    console.log(chalk.yellow("Aguarde... estamos criando a mensagem de commit."));
+    
     const result = await model.generateContent(prompt);
-    console.log(result);
-
+    
+    console.log(chalk.green("Mensagem gerada com sucesso: ",result.response?.text()));
     if (result?.response?.text()) {
       return result.response.text().trim(); // Retorna o texto gerado, removendo espaços extras
     } else {
